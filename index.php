da976011a4c103eb5686f56f63dbb97688fdeed6
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-06-16 20:04:14 +0200

    ADD logic

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dischi</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+
 </head>
 <body>
 
@@ -12,38 +14,25 @@
     </header>
 
     <?php
-        $dischi=[
-            [
-                'titolo' => 'ciao',
-                'artista' => 'ciao',
-                'cover' => 'ciao',
-                'anno' => 'ciao',
-                'genere' => 'ciao'
-            ],
-            [
-                'titolo' => 'ciao',
-                'artista' => 'ciao',
-                'cover' => 'ciao',
-                'anno' => 'ciao',
-                'genere' => 'ciao'
-            ],
-            [
-                'titolo' => 'ciao',
-                'artista' => 'ciao',
-                'cover' => 'ciao',
-                'anno' => 'ciao',
-                'genere' => 'ciao'
-            ],
-            [
-                'titolo' => 'ciao',
-                'artista' => 'ciao',
-                'cover' => 'ciao',
-                'anno' => 'ciao',
-                'genere' => 'ciao'
-            ]
-        ] 
                 
-        $json_string= json_encode($dischi)
+        $string = file_get_contents('./dischi.json');
+
+        $dischi = json_decode($string);
+
+        var_dump($dischi);
+
     ?>
+
+    <main>
+        <div class="container text-center">
+            <div class="row mt-2">
+                <?php
+                    foreach($dischi as $disco) {
+                        echo "<div class='col'> $disco </div>";
+                    }
+                ?>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
